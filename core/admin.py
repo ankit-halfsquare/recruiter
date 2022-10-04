3b45029bccdfa7a287fbd93e96558046841e4857
ankit <ankit.kalal.halfsquare@gmail.com>
2022-10-04 15:00:44 +0530

    initail commit

---
+++ core/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('project_division', 'positionname', 'desired_keywords','target_start_date','est_completion_date','assigneeid','actual_start_date','active','actual_completion_date','assigner','date_added','comments','statusid')
+
+class CandidateTableAdmin(admin.ModelAdmin):
+    list_display = ('candidate_id','first_name','last_name','state','phone','email','city','history','active','active_assignment','adder','date_added','fk_ra_company','fk_ra_project','fk_ra_position','skill_keywords','country','candidateFileName','candidateFileContents','candidateFileNameOriginal','candidateFileNamePDF','fileUploadDate','candidateFileNameOriginalFull','fileUploadUser','archive','skill_keywords_full')
+
+class HelpTextAdmin(admin.ModelAdmin):
+    list_display = ('Help_Text_ID','Help_ID_Name','Help_Text')
+
+
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ('keyword_id','text')
+
+
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ('key','objectType','dateStamp','componentGUID','userId','data','securityGroups')
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('status_name','status_description')
+
+
+class SynonymAdmin(admin.ModelAdmin):
+    list_display = ('synonym','keywordid')
+
+
+admin.site.register(Assignment,AssignmentAdmin)
+admin.site.register(CandidateTable,CandidateTableAdmin)
+admin.site.register(HelpText,HelpTextAdmin)
+admin.site.register(Keyword,KeywordAdmin)
+admin.site.register(Repository,RepositoryAdmin)
+admin.site.register(Status,StatusAdmin)
+admin.site.register(Synonym,SynonymAdmin)
---
+++ core/models.py
@@ -0,0 +1,131 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Assignment(models.Model):
+    project_division = models.CharField(max_length=255, blank=True, null=True)
+    positionname = models.CharField(max_length=50, blank=True, null=True)
+    desired_keywords = models.CharField(max_length=50, blank=True, null=True)
+    target_start_date = models.DateField(null=True, blank=True)
+    est_completion_date = models.DateField(null=True, blank=True)
+    assigneeid = models.IntegerField(null=True, blank=True)
+    actual_start_date = models.DateField(null=True, blank=True)
+    active = models.IntegerField(null=True, blank=True)
+    actual_completion_date = models.DateField(null=True, blank=True)
+    assigner = models.CharField(max_length=128, null=True, blank=True)
+    date_added = models.DateField(null=True, blank=True)
+    comments = models.CharField(max_length=255, blank=True, null=True)
+    statusid = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.positionname
+
+    class Meta:
+        managed = False
+        db_table = 'assignment'
+
+
+class CandidateTable(models.Model):
+    candidate_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50, blank=True, null=True)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    history = models.CharField(max_length=500, blank=True, null=True)
+    active = models.IntegerField(blank=True, null=True)
+    active_assignment = models.CharField(max_length=50, blank=True, null=True)
+    adder = models.IntegerField(blank=True, null=True)
+    date_added = models.DateTimeField(blank=True, null=True)
+    fk_ra_company = models.CharField(max_length=255, blank=True, null=True)
+    fk_ra_project = models.CharField(max_length=255, blank=True, null=True)
+    fk_ra_position = models.CharField(max_length=255, blank=True, null=True)
+    skill_keywords = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    candidateFileName = models.CharField(max_length=255, blank=True, null=True)
+    candidateFileContents = models.TextField(blank=True, null=True)
+    candidateFileNameOriginal = models.CharField(
+        max_length=255, blank=True, null=True)
+    candidateFileNamePDF = models.CharField(
+        max_length=255, blank=True, null=True)
+    fileUploadDate = models.DateTimeField(blank=True, null=True)
+    candidateFileNameOriginalFull = models.CharField(max_length=255, null=True, blank=True)
+    fileUploadUser = models.CharField(max_length=255, null=True, blank=True)
+    archive = models.IntegerField(null=True, blank=True)
+    skill_keywords_full = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        managed = False
+        db_table = 'candidate_tbl'
+
+
+class HelpText(models.Model):
+    Help_Text = models.TextField(max_length=255, blank=True, null=True)
+    Help_Text_ID = models.AutoField(primary_key=True)
+    Help_ID_Name = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.Help_ID_Name
+
+    class Meta:
+        managed = False
+        db_table = 'help_text'
+
+
+class Keyword(models.Model):
+    keyword_id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        managed = False
+        db_table = 'keyword'
+
+
+class Repository(models.Model):
+    key = models.AutoField(primary_key=True)
+    objectType = models.CharField(max_length=40, blank=True, null=True)
+    dateStamp = models.DateTimeField()
+    componentGUID = models.CharField(max_length=40, blank=True, null=True)
+    userId = models.CharField(max_length=40, blank=True, null=True)
+    data = models.TextField()
+    securityGroups = models.TextField()
+
+    def __str__(self):
+        return self.objectType
+
+    class Meta:
+        managed = False
+        db_table = 'repository'
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=50, blank=True, null=True)
+    status_description = models.CharField(
+        max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        managed = False
+        db_table = 'status'
+
+
+class Synonym(models.Model):
+    synonym = models.CharField(max_length=128, blank=True, null=True)
+    keywordid = models.IntegerField()
+
+    def __str__(self):
+        return self.synonym
+
+    class Meta:
+        managed = False
+        db_table = 'synonym'
