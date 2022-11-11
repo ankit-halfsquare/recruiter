f8f109c3ddbaa165bf1e36642b2c73d98fb9f1e9
ankit <ankit.kalal.halfsquare@gmail.com>
2022-11-11 15:00:54 +0530

    test

--- core/api/v091/views.py
+++ core/api/v091/views.py
@@ -79,38 +79,39 @@ class CandidateListCreateAPIView(generics.ListCreateAPIView):
 
 class CandidateView(APIView):
     def get(self, request,pk=None, *args, **kwarg):
+        print("=====>")
+        # def strtolst(obj):
+        #     if obj and "," in obj:
+        #         return obj.split(",")
+        #     return [obj] if obj else []
 
-        def strtolst(obj):
-            if obj and "," in obj:
-                return obj.split(",")
-            return [obj] if obj else []
+        # queryprms = request.GET
 
-        queryprms = request.GET
+        # skills = queryprms.get('skills',"")
+        # city = queryprms.get('city',"")
+        # exclude = queryprms.get('exclude',"") 
 
-        skills = queryprms.get('skills',"")
-        city = queryprms.get('city',"")
-        exclude = queryprms.get('exclude',"") 
-
-        if queryprms.get('search'):
-            searchname = queryprms.get('search')
+        # if queryprms.get('search'):
+        #     searchname = queryprms.get('search')
         
-            query = f"?skills={skills}&city={city}&exclude={exclude}"
+        #     query = f"?skills={skills}&city={city}&exclude={exclude}"
             
      
-        if skills or city or exclude:
-            skills = strtolst(skills) 
-            city = strtolst(city) 
-            exclude = strtolst(exclude) 
+        # if skills or city or exclude:
+        #     skills = strtolst(skills) 
+        #     city = strtolst(city) 
+        #     exclude = strtolst(exclude) 
             
-            q_object1 = reduce(or_, (Q(skill_keywords_full__icontains=key) for key in skills)) if skills else Q(skill_keywords_full__icontains = "")
-            q_object2 = reduce(or_, (Q(city__icontains=key) for key in city)) if city else Q(city__icontains = "")
-            q_object3 = reduce(or_, (~Q(skill_keywords_full__icontains=key) for key in exclude)) if exclude else ~Q(skill_keywords_full__icontains = "dummyValue")
+        #     q_object1 = reduce(or_, (Q(skill_keywords_full__icontains=key) for key in skills)) if skills else Q(skill_keywords_full__icontains = "")
+        #     q_object2 = reduce(or_, (Q(city__icontains=key) for key in city)) if city else Q(city__icontains = "")
+        #     q_object3 = reduce(or_, (~Q(skill_keywords_full__icontains=key) for key in exclude)) if exclude else ~Q(skill_keywords_full__icontains = "dummyValue")
             
-            q = (Q(q_object1) &Q(q_object2) & Q(q_object3))
-            Candidates = CandidateTable.objects.filter(q)
-        else:
-            Candidates = CandidateTable.objects.all()
+        #     q = (Q(q_object1) &Q(q_object2) & Q(q_object3))
+        #     Candidates = CandidateTable.objects.filter(q)
+        # else:
+        #     Candidates = CandidateTable.objects.all()
 
+        Candidates = CandidateTable.objects.all()
         serializer = CandidateTableSerializer(Candidates, many =True)
 
         return Response({ "data":serializer.data})
